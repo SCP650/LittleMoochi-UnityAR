ea504938ce70b3180afde97c7f00d198c024b7c7
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-06-17 15:34:38 -0400

    ar prototype ready

---
+++ Assets/Scripts/TapToPlaceObj.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class TapToPlaceObj : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager arRaycast;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycast = FindObjectOfType<ARRaycastManager>();
+    }
+ 
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arRaycast.Raycast(screenCenter, hits,UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+       }
+    }
+    void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
